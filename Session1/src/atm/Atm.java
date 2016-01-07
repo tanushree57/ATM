ed01a21eaa9e07b1a90d073540ca479bfb9dd3c4
Tanushree <tanu.angel57@gmail.com>
2016-01-07 10:34:18 +0530

    Updated atm program

--- Session1/src/atm/Atm.java
+++ Session1/src/atm/Atm.java
@@ -1,70 +1,47 @@
 package atm;
-import java.util.*;
 
 public class Atm {
-
-	private int balance = 100;
-	Scanner input = new Scanner(System.in);
 	
-	public int account() {
-		System.out.println("Enter account number");
-		int account;
-		account = input.nextInt();
-		return account;
+	//controller
+
+	BankAccount myaccno;
+	CardReader cardReader;
+	int accNo;
+		
+	public Atm(BankAccount myaccount) {
+		this.myaccno = myaccount;
+		cardReader = new CardReader();
+	}
+	
+	public boolean insertCard(Card card, int pin) {
+		cardReader.setCard(card);
+		if (cardReader.getCardNo() == myaccno.getCardNo()) {
+
+			if (cardReader.checkPin(pin)) {
+				accNo = myaccno.getAccNo();
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public float checkBalance() {
+		if (accNo != 0)
+			return myaccno.getBalance();
+		else
+			return 0;
+	}
+
+	
+	public boolean withdraw(float amount) {
+		if (accNo != 0) {
+				return myaccno.withdraw(amount);
+		}
+		else
+			return false;
+	}
+
 }
-	
-	public int pin() {
-		System.out.println("Enter your pin number: ");
-		int pin;
-		pin = input.nextInt();
-		return pin;
-	}
-	
-	public void atm() {
-		int ch;
-		System.out.println("ATM Machine");
-		System.out.println("1. View balance");
-		System.out.println("2. Withdraw cash");
-		System.out.println("Input choice: ");
-		ch = input.nextInt();
-		
-		switch(ch){
-		case 1: viewbal();
-		break;
-		case 2: withdraw(20);
-		break;
-		default: System.out.println("Wrong input.");
-		break;
-		}
-	}
-	
-	public void viewbal() {
-		System.out.println("Your balance is: Rs. "+ balance);
-	}
-	
-	public void withdraw(int amt) {
-		if (balance - amt < 0)
-			System.out.println("Insufficient funds");
-		else {
-			balance = balance - amt;
-			viewbal();
-		}
-	}
-	
-	public void startAtm() {
-		account();
-		pin();
-		atm();
-	}
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		BankAccount myaccount = new BankAccount(1234, 12, 100, 1000);
-		
-		Atm myAtm = new Atm();
-		myAtm.startAtm();
-		}
-	}
-
--- Session1/src/atm/CardReader.java
+++ Session1/src/atm/CardReader.java
@@ -2,6 +2,24 @@ package atm;
 
 public class CardReader {
 	
-	int cardno = 1000;
+	Card card;
+	
+
+	
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	public boolean checkPin(int pin) {
+		if (card.getPin() == pin)
+			return true;
+		else
+			return false;
+	}
+
+	public int getCardNo() {
+		return card.getCardNo();
+	}
+	
 
 }
